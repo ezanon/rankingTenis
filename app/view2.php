e1d5ccab3e500b794769d4727d2168897bce5b8e
Erickson Zanon <ezanon@gmail.com>
2018-09-03 11:16:10 -0300

    Issues #13 e #14 ok

--- app/view2.php
+++ app/view2.php
@@ -3,8 +3,10 @@
 class view2 {
 	
 	public function load($module,$action,$data=''){
-		include("views/$module/$action.tpl.php");
-		return true;
+            include('views/header2.tpl.php');
+            include("views/$module/$action.tpl.php");
+            include('views/footer2.tpl.php');
+            return true;
 	}
 
 }
---
+++ views/footer2.tpl.php
@@ -0,0 +1,12 @@
+    
+</div>
+    
+<!-- RODAPE -->
+<footer class="footer bg-dark">
+    <div class="container">
+        <span class="text-muted"><center>RankingTenis v.<?php echo $version; ?> :: <a href=https://github.com/ezanon/rankingTenis>GitHub</a>  © 2012-<?php echo date('Y'); ?> :: Erickson Zanon :: ezanon@gmail.com</center></span>
+    </div>
+</footer>
+<!-- RODAPE -->
+    
+</html>
