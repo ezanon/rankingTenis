38631afcf852799ef68b3d13caf49aceddb55ab2
Erickson Zanon <czanon@usp.br>
2022-09-16 16:26:04 -0300

    ajustes

--- views/fe/showTorneio2022.tpl.php
+++ views/fe/showTorneio2022.tpl.php
@@ -1,34 +1,40 @@
-<div id="torneio" class="container-fluid">
+<div id="torneio" class="container">
+
+    <div class="card">
+      <div class="card-header">
+        Torneio 2022
+      </div>
+      <div class="card-body">
+        <h5 class="card-title">Caso esteja com dificuldades para visualizar as chaves na sequência, clique no link abaixo para acessar a agenda e chaves do torneio no Google Planilhas.</h5>
+        <p class="card-text">
+            <a target="_blank" href="https://docs.google.com/spreadsheets/d/1K0-ifScGRJD-jRfVRiWbHepmMipRUEx3/edit?usp=sharing&ouid=101343888583349103051&rtpof=true&sd=true" class="btn btn-primary">Clique aqui!</a>
+        </p>
+      </div>
+    </div>
     
-<style type="text/css">
-#iframe-holder {
-    overflow:hidden;
-    position: relative;
-    top:0px;
-    bottom:0px;
-    left:0;
-    right:0;
- }
-#iframe-holder iframe {
-    width:100%;
-    height:100%;
- }
-</style>
-
-<div class="card">
-  <div class="card-header">
-    Torneio 2022
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Clique no link abaixo para acessar a agenda e chaves do torneio.</h5>
-    <p class="card-text">
-        <a target="_blank" href="https://docs.google.com/spreadsheets/d/1K0-ifScGRJD-jRfVRiWbHepmMipRUEx3/edit?usp=sharing&ouid=101343888583349103051&rtpof=true&sd=true" class="btn btn-primary">Clique aqui!</a>
-    </p>
-  </div>
 </div>
 
-<div id="iframe-holder">
-    <iframe src="https://docs.google.com/spreadsheets/d/1K0-ifScGRJD-jRfVRiWbHepmMipRUEx3/edit?usp=sharing&ouid=101343888583349103051&rtpof=true&sd=true" frameborder="0"  name="main" scrolling="no"></iframe>
-</div>
+<hr>
+
+<div id="torneio" class="container-fluid">
+
+    <style type="text/css">
+    #iframe-holder {
+        overflow:hidden;
+        position: relative;
+        top:0px;
+        bottom:0px;
+        left:0;
+        right:0;
+     }
+    #iframe-holder iframe {
+        width:100%;
+        height:100%;
+     }
+    </style>
+
+    <div id="iframe-holder">
+        <iframe src="https://docs.google.com/spreadsheets/d/1K0-ifScGRJD-jRfVRiWbHepmMipRUEx3/edit?usp=sharing&ouid=101343888583349103051&rtpof=true&sd=true" frameborder="0"  name="main" scrolling="no"></iframe>
+    </div>
 
 </div>
