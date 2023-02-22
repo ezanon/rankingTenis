f10e28f7db9e77c691b858c025446e6611323769
Erickson Zanon <czanon@usp.br>
2023-02-22 15:31:08 -0300

    Adicionado pdf FAQs

--- views/fe/showHome.tpl.php
+++ views/fe/showHome.tpl.php
@@ -1,6 +1,7 @@
 <div id="regulamento" class="container">
 
-<div class="card">
+ <!--
+ <div class="card">
   <div class="card-header">
     Importante
   </div>
@@ -11,6 +12,23 @@
         Quaisquer dúvidas, fale com o Prof. Thales. (thalesbon@yahoo.com.br)</p>
     <a href="docs/Regulamento-oficial-RANKING-2017.pdf" class="btn btn-primary">Clique aqui para abrir o arquivo.</a>
   </div>
+</div> 
+ -->
+    
+<div class="card">
+  <div class="card-header">
+    Importante
+  </div>
+  <div class="card-body">
+      <h5 class="card-title">FAQs (perguntas frequentes) do Funcionamento do Ranking</h5>
+    <p class="card-text">
+        Leia o texto na íntegra para participar do ranking.<br>
+        Quaisquer dúvidas sobre as regras, fale com Caio Tieppo (caiogra@gmail.com)
+    </p>
+    <a href="docs/FAQs-do-Funcionamento-do-Ranking.pdf" class="btn btn-primary">Clique aqui para abrir o arquivo.</a>
+  </div>
 </div>
+    
+    
 
 </div>
