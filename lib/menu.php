99667d0e2b03c2a27680053ffbe56e01c726e2aa
Erickson Zanon <ezanon@gmail.com>
2025-01-09 17:54:29 -0300

    gambi-menu

--- lib/menu.php
+++ lib/menu.php
@@ -3,8 +3,41 @@
 class menu {
 	
 	public $rodada;
+        
+        function imprimir(){
+// tirei o teste de SESSION pois estava apresentando erro (gambi)
+		$str = '';
+			$str.= "<menu>
+					  <strong>admin:</strong> 
+					  <a href=?module=admin&action=listagem>classificação</a> |";
+			// rodadas
+			$this->rodada = new rodada();
+			if ($this->rodada->rodada_atual==1)	
+				$str.= " <a href=?module=admin&action=rodada_atual>rodada atual</a> |";
+			else
+				$str.= " <a href=?module=admin&action=nova_rodada>nova rodada</a> |"; 
+			//ultima rodada
+			$str.= " <a href=?module=admin&action=ultima_rodada>ultima rodada</a> |";
+			//arquivos
+			$str.= " <a href=?module=admin&action=arquivos>obter arquivos</a> |";
+			//novo jogador
+			
+				// se a rodada está em andamento, não libera a criação de novo jogador
+				$rodada = new rodada();
+				if ($rodada->nova_rodada == 1){
+					$str.= " <a href=?module=admin&action=novo_jogador>novo jogador</a> |";
+				}
+				$rodada = NULL;
 
-	function imprimir(){
+			//funcoes
+			$str.= " <a href=?module=admin&action=funcoes>funcoes</a> |";
+			// sair 
+			$str.= "<a href=?module=login&action=sair>sair</a>
+					</menu>";
+		return $str;
+	}
+
+	function imprimirOld(){
 
 		$str = '';
 		if (@$_SESSION['jogador']['jogador']){
